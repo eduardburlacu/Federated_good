ae2f398dfc09f8eb220bca62a776c441fc0fee80
eduardburlacu <eduardburlacu@Dellescu>
2023-07-18 16:24:32 +0100

    config partial integration

--- src/script/parse_config.py
+++ src/script/parse_config.py
@@ -10,20 +10,31 @@ class DatasetNode():
     if dataset=='CIFAR10':
         self.dataset = torchvision.datasets.CIFAR10
         self.task = 'vision'
-        self.shape = (3, 32, 32)
+        self.shape = (3,32,32)
         self.size =60000
         self.classes = ("plane", "car", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck",)
 
     elif dataset=='MNIST':
         self.dataset = torchvision.datasets.MNIST
         self.task = 'vision'
-        self.shape = (1,28, 28)
+        self.shape = (1,28,28)
         self.size = 60000
         self.classes = tuple(i for i in range(1,10))
-  def __str__(self):
-    return self.name
 
-def get_variables(file:str = 'mock', sim=False):
+    elif dataset=='FEMNIST':
+        self.dataset = None
+        self.task = 'vision'
+        self.shape =(1,28,28)
+        self.size = 805263
+        self.classes = tuple("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
+
+    else: raise ValueError('DatasetNode is broken')
+
+    self.num_classes = len(self.classes)
+
+  def __repr__(self): return self.name
+
+def get_variables(file:str = 'mock'):
     '''
     Dictionary:
     ---> Federated variables
@@ -41,12 +52,11 @@ def get_variables(file:str = 'mock', sim=False):
     BATCH_SIZE = config['BATCH_SIZE']
     EPOCHS = config['EPOCHS']
     '''
-    if sim: path = os.path.join(PATH_sim,file+'.toml')
-    else: path = os.path.join(PATH['config'],file+'.toml')
+    try: path = os.path.join(PATH['config'],file+'.toml')
+    except: path = os.path.join(PATH_sim, file + '.toml')
+
     with open(path, 'r') as config_file:
         config_var = toml.load(config_file)
     config_var['DATASET'] = DatasetNode(config_var['DATASET'])
+
     return config_var
-
-
-
